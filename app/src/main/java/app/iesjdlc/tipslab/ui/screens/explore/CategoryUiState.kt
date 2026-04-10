a19933c1bc13d11a9809ef5bc6a933949f2678ec
malmorox <marcos.almorox@gmail.com>
2026-04-10 13:42:26 +0200

    Añadiendo estados a varios ViewModels para que no haya errores de compilación (se cambiará en el futuro)

--- app/src/main/java/app/iesjdlc/tipslab/ui/screens/explore/CategoryUiState.kt
+++ app/src/main/java/app/iesjdlc/tipslab/ui/screens/explore/CategoryUiState.kt
@@ -1,3 +1,9 @@
 package app.iesjdlc.tipslab.ui.screens.explore
 
-data class CategoryUiState()
+data class CategoryUiState(
+    val emailOrUsername: String = "",
+    val password: String = "",
+    val isPasswordVisible: Boolean = false,
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null
+)
--- app/src/main/java/app/iesjdlc/tipslab/ui/screens/explore/ExploreUiState.kt
+++ app/src/main/java/app/iesjdlc/tipslab/ui/screens/explore/ExploreUiState.kt
@@ -1,3 +1,9 @@
 package app.iesjdlc.tipslab.ui.screens.explore
 
-data class ExploreUiState()
+data class ExploreUiState(
+    val emailOrUsername: String = "",
+    val password: String = "",
+    val isPasswordVisible: Boolean = false,
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null
+)
