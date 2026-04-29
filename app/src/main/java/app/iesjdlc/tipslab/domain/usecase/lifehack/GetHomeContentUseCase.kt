d6d35a89b876ade8bfeb50a1688e58ec3c32d889
malmorox <marcos.almorox@gmail.com>
2026-04-29 10:21:17 +0200

    Creando nuevos casos de uso

---
+++ app/src/main/java/app/iesjdlc/tipslab/domain/usecase/lifehack/GetHomeContentUseCase.kt
@@ -0,0 +1,12 @@
+package app.iesjdlc.tipslab.domain.usecase.lifehack
+
+import app.iesjdlc.tipslab.domain.repository.LifehackRepository
+import javax.inject.Inject
+
+class GetHomeContentUseCase @Inject constructor(
+    private val lifehackRepository: LifehackRepository
+) {
+    suspend operator fun invoke() {
+
+    }
+}
--- app/src/main/java/app/iesjdlc/tipslab/domain/usecase/lifehack/GetLifehacksByCategoryUseCase.kt
+++ app/src/main/java/app/iesjdlc/tipslab/domain/usecase/lifehack/GetLifehacksByCategoryUseCase.kt
@@ -4,10 +4,20 @@ import app.iesjdlc.tipslab.domain.model.Lifehack
 import app.iesjdlc.tipslab.domain.repository.LifehackRepository
 import javax.inject.Inject
 
+data class CategorySectionLifehacks(
+    val recent: List<Lifehack>,
+    val popular: List<Lifehack>
+)
+
 class GetLifehacksByCategoryUseCase @Inject constructor(
     private val lifehackRepository: LifehackRepository
 ) {
-    suspend operator fun invoke(categoryId: Int): Result<List<Lifehack>> {
-        return lifehackRepository.getLifehacksByCategory(categoryId)
+    suspend operator fun invoke(categoryId: Int): Result<CategorySectionLifehacks> = runCatching {
+
+
+        CategorySectionLifehacks(
+            recent = ,
+            popular =
+        )
     }
 }
