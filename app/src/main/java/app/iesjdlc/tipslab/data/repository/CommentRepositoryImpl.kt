2e0956f4dbf928fb4f0d9d676a8523eda0736992
malmorox <marcos.almorox@gmail.com>
2026-05-14 18:48:17 +0200

    Implementación del repositorio de comentarios y vinculación en la inyección de dependencias

--- app/src/main/java/app/iesjdlc/tipslab/data/repository/CommentRepositoryImpl.kt
+++ app/src/main/java/app/iesjdlc/tipslab/data/repository/CommentRepositoryImpl.kt
@@ -1,4 +1,44 @@
 package app.iesjdlc.tipslab.data.repository
 
-class CommentRepositoryImpl {
+import app.iesjdlc.tipslab.domain.model.Comment
+import app.iesjdlc.tipslab.domain.repository.CommentRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class CommentRepositoryImpl @Inject constructor(
+
+) : CommentRepository {
+    override fun observeComments(lifehackId: String): Flow<List<Comment>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun addComment(
+        lifehackId: String,
+        text: String
+    ): Result<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun addReply(
+        lifehackId: String,
+        commentId: String,
+        text: String
+    ): Result<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteComment(
+        lifehackId: String,
+        commentId: String
+    ): Result<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteReply(
+        lifehackId: String,
+        commentId: String,
+        replyId: String
+    ): Result<Unit> {
+        TODO("Not yet implemented")
+    }
 }
